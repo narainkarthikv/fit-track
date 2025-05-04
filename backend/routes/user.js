0137f7bf516f43d5d767d5201e30209bf3625198
Maheen Fatma <goodmaheen@gmail.com>
2025-05-04 15:35:02 +0530

    Route to fetch streak information

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -98,6 +98,25 @@ router.post('/login', async (req, res) => {
     }
 });
 
+//fetch streak info
+router.get('/streak/:userId', async(req,res) => {
+    const {userId} = req.params;
+
+    try {
+        const user = await User.findById(userId)
+
+        if(!user) return res.status(404).json({ error: 'User not found' });
+
+        res.json({
+            dayCheck: user.dayCheck,
+            streakCount: user.streakCount
+        });
+    } catch (error) {
+        console.error('Error fetching streak:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
 // Get all users
 router.get('/', async (req, res) => {
     try {
